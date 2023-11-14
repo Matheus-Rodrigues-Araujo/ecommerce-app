19d4a75366db8e13f8a612bf106fe4ecb8cf150a
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-11-14 15:05:21 -0300

    updated

--- app/components/products.jsx
+++ app/components/products.jsx
@@ -3,13 +3,11 @@ import useSWR from 'swr'
 import ProductCard from '../components/productCard'
 import { useGlobalContext } from '../context/store'
 import { useState } from 'react'
-// const api = process.env.NEXT_PUBLIC_API
-const api = process.env.NEXT_PUBLIC_API
 
 export default function Products(){
     const [purchaseList, setPurchaseList] = useState([])
     const fetcher = (...args) => fetch(...args).then(res => res.json())
-    const { data, error, isLoading } = useSWR(api+'?page=1&rows=8&sortBy=id&orderBy=DESC', fetcher)
+    const { data, error, isLoading } = useSWR('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC', fetcher)
     if (error) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
     const products = data && data.products && data.products;
